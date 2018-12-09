ff41774c7e3813f656868165da572f269d287fa1
mrhanafi <muhdhanafirahman@gmail.com>
2018-12-09 17:40:31 +0000

    modi read.py

--- read.py
+++ read.py
@@ -1,12 +1,12 @@
 import git, os, shutil
 
-DIR_NAME = "temp"
+DIR_NAME = "/home/pi/git/arduino"
 REMOTE_URL = "https://github.com/mrhanafi/arduino.git"
 
-if os.path.isdir(DIR_NAME):
-	shutil.rmtree(DIR_NAME)
+#if os.path.isdir(DIR_NAME):
+#	shutil.rmtree(DIR_NAME)
 
-os.mkdir(DIR_NAME)
+#os.mkdir(DIR_NAME)
 
 repo = git.Repo.init(DIR_NAME)
 origin = repo.create_remote('origin',REMOTE_URL)
