a6d1ad8272eb91a05f9d10c92212cc6df5b57d17
mrhanafi <muhdhanafirahman@gmail.com>
2018-12-09 17:47:51 +0000

    huhu1

--- read.py
+++ read.py
@@ -8,10 +8,12 @@ REMOTE_URL = "https://github.com/mrhanafi/arduino.git"
 
 #os.mkdir(DIR_NAME)
 
-repo = git.Repo.init(DIR_NAME)
+#repo = git.Repo.init(DIR_NAME)
+repo = git.Repo("/home/pi/git/arduino")
 #origin = repo.create_remote('origin',REMOTE_URL)
 origin = repo.remotes.origin
 origin.fetch()
-origin.pull(origin.refs[0].remote_head)
+#origin.pull(origin.refs[0].remote_head)
+origin.pull()
 
 print "-- Done --"
